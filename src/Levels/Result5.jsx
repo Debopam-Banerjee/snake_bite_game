818f6f62f8f5271d445224a0e721f17db8333958
Nilanjana Chaudhuri <145705332+Debopam-Banerjee@users.noreply.github.com>
2025-02-17 00:20:32 +0530

    Update Result5.jsx

--- src/Levels/Result5.jsx
+++ src/Levels/Result5.jsx
@@ -87,7 +87,7 @@ const FinalResult5 = () => {
         <>
           <div className="p-6 flex flex-col items-center">
             <h2 className="text-2xl font-bold text-amber-950 mb-6">Game Results</h2>
-            <p className="text-lg text-gray-600 mb-4 font-semibold" >
+            <p className="text-lg text-amber-600 mb-4 font-semibold" >
               The options you selected since Level 1
             </p>
             <div className="w-full max-w-lg bg-white p-4 rounded-lg shadow-lg">
@@ -107,13 +107,13 @@ const FinalResult5 = () => {
             <div className="flex space-x-2">
               <button
                 onClick={handleHomeClick}
-                className="bg-amber-700 text-white px-4 py-2 mt-6 rounded-md hover:bg-amber-800 transition"
+                className="bg-amber-800 text-white px-4 py-2 mt-6 rounded-md hover:bg-amber-900 transition"
               >
                 Home
               </button>
               <button
                 onClick={handleExitClick}
-                className="bg-amber-700 text-white px-4 py-2 mt-6 rounded-md hover:bg-amber-800 transition"
+                className="bg-amber-800 text-white px-4 py-2 mt-6 rounded-md hover:bg-amber-900 transition"
               >
                 Exit
               </button>
